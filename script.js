66edf1f7bb7da9fd6ada8a0db5c2f2f604b4fc02
Oscar Cantaro <oscar.cantaro@outlook.com>
2024-08-22 23:37:15 -0500

    se agrego boton de borrar ultimo  caracter

--- script.js
+++ script.js
@@ -12,3 +12,8 @@ function agregarALaPantalla(value) {
     document.getElementById('pantalla').value = '';
   }
   
+  function borrarultimo()
+  {
+    const valorPantalla = document.getElementById('pantalla').value;
+    document.getElementById('pantalla').value = valorPantalla.slice(0.-1);
+  }
