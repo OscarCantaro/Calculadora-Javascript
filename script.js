359d69628ba27ee394c366a03c19641aabdf8e93
Oscar Cantaro <oscar.cantaro@outlook.com>
2024-08-22 23:51:27 -0500

    actualizando el boton elimnar ultimo

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ function agregarALaPantalla(value) {
     document.getElementById('pantalla').value = '';
   }
   
+  function borrarUltimo()
+  {
+    const valorPantalla = document.getElementById('pantalla').value;
+    const resultado = valorPantalla.substring(0, valorPantalla.length -1);
+    document.getElementById('pantalla').value = resultado;
+  }
